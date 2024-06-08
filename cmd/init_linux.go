4626985866e1deaaafdb87688cacb10ba603a301
levi.tang <levi.tange@icloudsky.com>
2024-06-08 17:41:53 +0800

    [fix]添加容器获取cpu函数

---
+++ cmd/init_linux.go
@@ -0,0 +1,14 @@
+//go:build linux
+// +build linux
+
+package cmd
+
+/**
+@file:
+@author: levi.Tang
+@time: 2024/6/8 17:39
+@description: 添加容器获取cpu函数
+**/
+import (
+	_ "go.uber.org/automaxprocs"
+)
